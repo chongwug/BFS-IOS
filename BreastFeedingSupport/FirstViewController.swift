d857c5912cc335b840ce0d80b7ab0df8456cd1b7
chongwug <cguo@lawn-128-61-11-111.lawn.gatech.edu>
2017-03-15 16:28:04 -0400

    Initial Commit

---
+++ BreastFeedingSupport/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BreastFeedingSupport
+//
+//  Created by CHONG W GUO on 3/15/17.
+//  Copyright © 2017 CHONG W GUO. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BreastFeedingSupport/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BreastFeedingSupport
+//
+//  Created by CHONG W GUO on 3/15/17.
+//  Copyright © 2017 CHONG W GUO. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
